8910d7699c9fd64e01e3e39c274b3a11026493a7
Alfser <j.janilson12@gmail.com>
2019-07-02 16:34:18 -0300

    neural network with 4 % of error

--- neural networks/tb_redes_neurais.R
+++ neural networks/tb_redes_neurais.R
@@ -3,19 +3,27 @@
 #to verify the kind of eating to you deslike
 library('neuralnet')
 
-#separing set train and set test from csv file 
-table = read.table('set_of_test_train.csv', header = T, sep = ';', stringsAsFactors = F)
-train = table[1:15 , ]
-test =  table[16:20, ]
+#separing set train and set test from csv file
+#this set contain 81 exemples
+#contain 8 questions 5 class of out
+#the elements are variable from -3 fom 0 
 
-#normalizing sets
-train[, 1:8] = train[, 1:8]/3
-test[, 1:8] = test[, 1:8]/3
+table = read.table('set_of_train_and_test.csv', header = T, sep = ';', stringsAsFactors = F)
+#shuffleing lines to get more mixed
+shuffle_table = table[sample(nrow(table)),]
+
+#geting set o train end test shuffled
+train = shuffle_table[1:60 , ]
+test =  shuffle_table[61:81, ]
+
+#normalizing sets intervals from -3,0 to 0,1 
+train[, 1:8] = (train[, 1:8] - min(shuffle_table))/4
+test[, 1:8] = (test[, 1:8]- min(shuffle_table))/4
 
 names = names(table)
 
 #creating formula to put it into neurauralnet function 
-f = as.formula(paste(paste(names[9:11], collapse = '+'), '~',paste(names[1:8], collapse = '+')))
+f = as.formula(paste(paste(names[9:13], collapse = '+'), '~',paste(names[1:8], collapse = '+')))
 
 nn = neuralnet(formula = f, data = train, hidden = c(5,5), act.fct = 'logistic', linear.output = F)
 
